4e430b43cba0fe9dd34f40e2dba65c621373e4b3
talejo <tzalejo@gmail.com>
2020-04-22 05:03:48 -0300

    agrego filtro

--- app/Formulario.php
+++ app/Formulario.php
@@ -20,6 +20,7 @@ class Formulario extends Model
         'tipo_criptomoneda', 
         'importe_compra', 
         'fecha_compra', 
+        'fecha', 
         'dolar',
         'estado',
         'costo_criptomoneda_p',  // es cuanto sale la criptomoneda con respecto a la comision del proveedor (comision + criptomoneda)
@@ -42,6 +43,54 @@ class Formulario extends Model
         return $this->belongsTo(User::class);
     }
 
+    // Scope, los filtros que vamos a utilizar..
 
+    public function scopeCompra_moneda($query, $compra_moneda) {
+        if ($compra_moneda) {
+            # code...
+            $moneda = ucwords(strtolower($compra_moneda));
+            return $query->where('compra_moneda', 'LIKE' , "%$moneda%");
+        }
+        
+    }
 
+    public function scopeFecha($query, $fechaDesde, $fechaHasta) {
+        // '2020-04-13 15:14:29'
+        // DateTime::createFromFormat('Y-m-d H:i:s', '2020-04-13 15:14:29');
+        if ($fechaDesde) {
+            # code...
+            if ($fechaHasta) {
+                # code...
+                $miDesde = date($fechaDesde);
+                $miHasta = date($fechaHasta);
+                return $query->whereBetween('fecha', [$miDesde, $miHasta]);
+            }
+        }
+    }
+
+    public function scopeTipo_criptomoneda($query, $tipo_criptomoneda) {
+        if ($tipo_criptomoneda) {
+            # code...
+            $moneda = ucwords(strtolower($tipo_criptomoneda));
+            return $query->where('tipo_criptomoneda', 'LIKE' , "%$moneda%");
+        }
+        
+    }
+
+    public function scopeEstado($query, $estado) {
+        if ($estado) {
+            # code...
+            $estadoNuevo = strtolower($estado);
+            return $query->where('estado' ,$estadoNuevo);
+        }
+        
+    }
+
+    public function scopeCliente_id($query, $cliente_id) {
+        if ($cliente_id) {
+            # code...
+            return $query->where('cliente_id' ,$cliente_id);
+        }
+        
+    }
 }
