703d511c396821046e30b5b78e2e2036637fcc92
talejo <tzalejo@gmail.com>
2020-04-22 05:06:11 -0300

    actualizo el api

--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,7 @@ Route::group(['prefix' => 'cliente'], function () {
         Route::get('', 'Cliente\ClienteController@index');
         Route::get('mostrar/{cliente}', 'Cliente\ClienteController@show');
         Route::post('crear', 'Cliente\ClienteController@store'); # Creo un Cliente
+        Route::put('modificar/{cliente}', 'Cliente\ClienteController@update');
     });
 });
 # Group de ruta con prefijo alumnos, agrego cors a las rutas
@@ -44,7 +45,8 @@ Route::group(['prefix' => 'proveedor'], function () {
     # Rutas con middleware auth
     Route::group(['middleware' => ['auth:api']], function () {
         Route::get('', 'Proveedor\ProveedorController@index');
-        Route::post('crear', 'Proveedor\ProveedorController@store'); # Creo un Cliente
+        Route::post('crear', 'Proveedor\ProveedorController@store'); # Creo un proveedor
+        Route::put('modificar/{proveedor}', 'Proveedor\ProveedorController@update'); # Creo un proveedor
     });
 });
 # Group de ruta con prefijo formulario, agrego cors a las rutas
@@ -55,7 +57,7 @@ Route::group(['prefix' => 'formulario'], function () {
         // Route::get('mostrar', 'Formulario\FormularioController@show');
         Route::post('crear', 'Formulario\FormularioController@store'); # Creo un Formulario
         Route::put('modificar/{formulario}', 'Formulario\FormularioController@update'); # Modifico un estudiante
-        Route::delete('borrar/{formulario}','Formulario\FormularioController@destroy');
+        Route::delete('borrar/{formulario}', 'Formulario\FormularioController@destroy');
     });
 });
 
