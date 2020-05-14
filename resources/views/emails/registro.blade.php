fd901fe9147cc080c25563480c136f8cb277f4dd
tzalejo <tzalejo@gmail.com>
2020-05-13 21:50:35 -0300

    el texto que se envia por correo ya sea para registro o reset

---
+++ resources/views/emails/registro.blade.php
@@ -0,0 +1,10 @@
+<h2>Registracion Confirmada Sistema Bitcoin</h2>
+
+<p>Estimado/a {{$usuario['name']}} {{$usuario['apellido']}}</p>
+<p>Gracias por estar con nosotros.</p>
+<p>Tome nota de su nombre de usuario y password, ya que lo necesitara en cualquier momento:</p>
+
+<p><strong>Nombre Usuario: </strong> {{$usuario['email']}}</p> 
+<p><strong>Password: </strong> {{$usuario['password']}}</p>
+
+<p>Gracias por utilizar nuestro sistema</p>
---
+++ resources/views/emails/reset-password.blade.php
@@ -0,0 +1,9 @@
+<h2>Reset Password Sistema Bitcoin</h2>
+
+<p>Estimado/a {{$usuario['name']}} {{$usuario['apellido']}}</p>
+<p>Tome nota de su nombre de usuario y NUEVO PASSWORD, ya que lo necesitara en cualquier momento:</p>
+
+<p><strong>Nombre Usuario: </strong> {{$usuario['email']}}</p> 
+<p><strong>Password: </strong> {{$usuario['password']}}</p>
+
+<p>Gracias por utilizar nuestro sistema</p>
