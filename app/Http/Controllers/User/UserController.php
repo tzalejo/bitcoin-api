1d5ba30fb902fa41016ff3aa4901b69c35152eeb
tzalejo <tzalejo@gmail.com>
2021-08-02 01:21:03 -0300

    add: fix

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -9,58 +9,11 @@ use App\User;
 
 class UserController extends ApiController
 {
-    //
     use ApiResponser;
-    /**
-     * Devuelvo todas las comisiones activas(que no se cerraron por las fechas)
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    
+    public function index(): \Illuminate\Http\Response
     {
         return User::query()->orderBy('name','ASC')->get();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Comision  $comision
-     * @return \Illuminate\Http\Response
-     */
-    public function show($userId)
-    {}
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Comision  $comision
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, User $user)
-    {
-
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Comision  $comision
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy( User $user)
-    {
-
-    }
 }
